ad1cc4ffaba92a63613fecaa1e50e69cd585794e
EmmettSamoil <EmmettSamoil@users.noreply.github.com>
2020-10-19 13:32:57 -0600

    Update main.js

--- main.js
+++ main.js
@@ -4,20 +4,22 @@ document.getElementById('all').addEventListener('click', genAll);
 
 //Containers
 let nameArray = [" 'Crusher' ", " 'The Scientist' ", " 'Twinkle-Toes' ", " 'The Coder' ", " 'The jester' ", " 'The Sloth' ", " 'Quick-Silver' "];
-let randomName = nameArray[Math.floor(Math.random() * nameArray.length)];
 let firstName = document.getElementById('firstN');
 let lastName = document.getElementById('lastN');
 
 //Function
 function genRandom () {
+    let randomName = nameArray[Math.floor(Math.random() * nameArray.length)];
     // console.log( + nameArray[0])
    document.getElementById('output').innerHTML = firstName.value + randomName + lastName.value;
 }
 
 function genAll () {
+    document.getElementById('output').innerHTML = '';
     for (let i = 0; i < nameArray.length; i++) {
         console.log(nameArray[i]);
-        document.getElementById('output').innerHTML = firstName.value + nameArray + lastName.value;
+        document.getElementById('output').innerHTML += firstName.value + nameArray[i] + lastName.value + " , " ;
+        // document.write("\n"); 
     }
 }
 
